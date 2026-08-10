31ead1827f0f9a72833b9439704eb21b66c2b23a
NeoFoxi <technik.wunder@icloud.com>
2026-08-10 10:51:58 +0200

    fixed Schaufenster view image loading Bug.

--- web/schaufenster/view/index.js
+++ web/schaufenster/view/index.js
@@ -1,9 +1,9 @@
 const urlIdParams = new URLSearchParams(window.location.search);
 const urlId = urlIdParams.get('id');
 
-document.getElementById("img").src = `/schaufenster/api/object/${urlId}/img.jpg`;
+document.getElementById("img").src = `/schaufenster/data/object/${urlId}/img.jpg`;
 
-    fetch(`/schaufenster/api/object/${urlId}/properties.json`)
+    fetch(`/schaufenster/data/object/${urlId}/properties.json`)
 .then(response => response.json())
 .then(objectPropertiesjson => {
     document.getElementById("card-title").innerHTML = objectPropertiesjson.title;
